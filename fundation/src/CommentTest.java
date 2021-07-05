4fa1503024fb766649c28d35a89bcb2accb315e5
panjie <260313538@.com>
2021-07-05 21:57:54 +0800

    add fundation1

---
+++ fundation/src/CommentTest.java
@@ -0,0 +1,9 @@
+public class CommentTest {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        // 不会编译
+        int age=24;
+        String aa = "hello" + " java";
+        System.out.println(age+aa);
+    }
+}
