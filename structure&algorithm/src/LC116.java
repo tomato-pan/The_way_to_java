2abb93b1f6bfd74e6be8a052c0a8dd82d3fd6706
panj <pj_uestc@163.com>
2021-07-12 11:13:27 +0800

    modify 结构

--- structure&algorithm/src/LC116.java
+++ structure&algorithm/src/LC116.java
@@ -1,22 +1,3 @@
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
 public class LC116 {
     public Node connect(Node root) {
         if (root==null){
@@ -33,5 +14,28 @@ public class LC116 {
         connectTwoNode(node1.left,node1.right);
         connectTwoNode(node1.right,node2.left);
         connectTwoNode(node2.left,node2.right);
+    }
+    void flatten(TreeNode root) {
+        // base case
+        if (root == null) return;
+
+        flatten(root.left);
+        flatten(root.right);
+
+        /**** 后序遍历位置 ****/
+        // 1、左右子树已经被拉平成一条链表
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        // 2、将左子树作为右子树
+        root.left = null;
+        root.right = left;
+
+        // 3、将原先的右子树接到当前右子树的末端
+        TreeNode p = root;
+        while (p.right != null) {
+            p = p.right;
+        }
+        p.right = right;
     }
 }
--- structure&algorithm/src/binTree1.java
+++ structure&algorithm/src/binTree1.java
@@ -1,22 +1,3 @@
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 public class binTree1 {
     public int count(TreeNode root) {
         if (root == null) return 0;
