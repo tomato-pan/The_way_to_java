a3c82ea8b51962f12077124c7fa58d0c5b51ebef
panj <pj_uestc@163.com>
2021-07-14 10:31:53 +0800

    add the different of String\StringBuffer\StringBulider

---
+++ fundation/src/stringdemo1.java
@@ -0,0 +1,24 @@
+public class stringdemo1 {
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = "abc";
+        // 此时s1、s2指向同一个"abc"对象
+        s1 = s1.concat("efg");// 此时s1-->新的对象"abcefg"
+        // 当想用可修改的字符串-->StringBuffer or StringBuilder
+        System.out.println(s1);
+        // 单线程常用 StringBulider
+        StringBuilder sb = new StringBuilder();
+        sb.append("aa");
+        sb.append("cc");
+        sb.insert(1,"abc");
+        System.out.println(sb);
+        sb.delete(0,1);
+        System.out.println(sb);
+        // 多线程安全使用StringBuffer
+        StringBuffer sBuffer = new StringBuffer("learning java: ");
+        sBuffer.append("www");
+        sBuffer.append(".javaapi");
+        sBuffer.append(".com");
+        System.out.println(sBuffer);
+    }
+}
