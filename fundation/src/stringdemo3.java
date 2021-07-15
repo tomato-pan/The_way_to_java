2194986c940a5d283c92b82587739cb04fb3aa1c
panj <pj_uestc@163.com>
2021-07-15 13:58:28 +0800

    add switch对string的支持方式

--- fundation/src/stringdemo3.java
+++ fundation/src/stringdemo3.java
@@ -10,5 +10,11 @@ public class stringdemo3 {
         // sb>s1
         System.out.println(sb);
         System.out.println(s1);
+        // String.valueOf or Integer.toString 方式 基本相同
+        // s2+aa 这种方式是 = (new StringBuilder()).append(aa).toString() 实现的
+        int aa = 5;
+        String s2 = "abc";
+        System.out.println(String.valueOf(aa)+s2);
+        System.out.println(s2+aa);
     }
 }
