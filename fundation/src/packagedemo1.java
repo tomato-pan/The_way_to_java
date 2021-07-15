efdbffaceb6ef63a4e5ee4e34b9e669a9d0f260e
panj <pj_uestc@163.com>
2021-07-15 17:51:32 +0800

    add 拆装箱

--- fundation/src/packagedemo1.java
+++ fundation/src/packagedemo1.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class packagedemo1 {
     public static void main(String[] args) {
         int a1=221223;
         short a2 = 12312;
-        byte a3 = 129;
+        byte a3 = 127;
         long a4 = 12312312312L;
+        // 自动拆装箱的场景  将基础类型放入集合里---此时会自动装箱
+        List<Integer> li = new ArrayList<>();
+        for (int i=1;i<50;i++){
+            li.add(i);
+        }
 
     }
 }
