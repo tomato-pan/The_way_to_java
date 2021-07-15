aa8cdd12d6030735a6d20b7fa1abddbe2ef3cad6
panj <pj_uestc@163.com>
2021-07-15 17:23:38 +0800

    add 拆装箱

---
+++ fundation/src/packagedemo1.java
@@ -0,0 +1,9 @@
+public class packagedemo1 {
+    public static void main(String[] args) {
+        int a1=221223;
+        short a2 = 12312;
+        byte a3 = 129;
+        long a4 = 12312312312L;
+
+    }
+}
