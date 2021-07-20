5d3004e55040ec700ee8b3ff935015b092bbf856
panj <pj_uestc@163.com>
2021-07-20 18:17:30 +0800

    add static

--- fundation/src/staticdemo1.java
+++ fundation/src/staticdemo1.java
@@ -9,10 +9,21 @@ public class staticdemo1 {
         System.out.println(sd1.number);
         System.out.println(sd.number);
         System.out.println(staticdemo.number);
+        staticdemo.setNum(12345);
+        System.out.println(staticdemo.getNum());
     }
 }
-class staticdemo{
+
+class staticdemo {
     public String name;
     public int age;
     public static int number; //静态字段属于类，并不属于实例
+    private static int num; //静态字段属于类，并不属于实例
+
+    public static void setNum(int numVal) {
+        num = numVal;
+    }
+    public static int getNum() {
+        return num;
+    }
 }
