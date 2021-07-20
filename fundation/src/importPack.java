b0ea1567c8f686cdb6bfcc6ce1988765bf0aaebf
panj <pj_uestc@163.com>
2021-07-20 18:28:08 +0800

    add static

---
+++ fundation/src/importPack.java
@@ -0,0 +1,12 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class importPack {
+    public static void main(String[] args) {
+        Map<String,String> mp = new HashMap<>();
+        mp.put("11","23");
+        System.out.println(mp.get("11"));
+    }
+}
