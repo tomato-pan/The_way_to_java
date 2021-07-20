48b765ccc7b5892fe956d35f6e693fef992b6d9b
panj <pj_uestc@163.com>
2021-07-20 17:34:04 +0800

    add abstract class

---
+++ fundation/src/interfacedemo1.java
@@ -0,0 +1,27 @@
+public class interfacedemo1 {
+    public static void main(String[] args) {
+        Bens bb = new Bens("gls350");
+        bb.run();
+        System.out.println(bb.getName());
+    }
+}
+interface Car{
+    void run();
+    String getName();
+}
+class Bens implements Car{
+    private String name;
+    public Bens(String name){
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(this.name+" running!");
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
