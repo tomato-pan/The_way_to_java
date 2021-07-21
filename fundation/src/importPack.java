6cb11ebbddb2de1a904ae3af242d4d45ddf8616a
panj <pj_uestc@163.com>
2021-07-21 15:29:30 +0800

    add 内部类/匿名类

--- fundation/src/importPack.java
+++ fundation/src/importPack.java
@@ -8,5 +8,33 @@ public class importPack {
         Map<String,String> mp = new HashMap<>();
         mp.put("11","23");
         System.out.println(mp.get("11"));
+        // 内部类
+        Outer outer = new Outer("aaa");
+        Outer.Inner1 inner = outer.new Inner1();
+        inner.hello();
+        outer.asyncHello();
+    }
+}
+class Outer {
+    private String name;
+
+    Outer(String name) {
+        this.name = name;
+    }
+
+    class Inner1 {
+        void hello() {
+            System.out.println("Hello, " + Outer.this.name);
+        }
+    }
+    // Anonymous Class
+    void asyncHello() {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello, " + Outer.this.name);
+            }
+        };
+        new Thread(r).start();
     }
 }
