46c072134126d0c8bbe1be3d9505d14561bf9d6f
panj <pj_uestc@163.com>
2021-08-18 13:57:03 +0800

    add reflect

---
+++ fundation/src/reflectDemo.java
@@ -0,0 +1,55 @@
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+public class reflectDemo {
+    public static void test() throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        Class<?> c = methodClass.class;
+        Object object = c.newInstance();
+        Method[] methods = c.getMethods();
+        Method[] declaredMethods = c.getDeclaredMethods();
+        //获取methodClass类的add方法
+        Method method = c.getMethod("add", int.class, int.class);
+        //getMethods()方法获取的所有方法
+        System.out.println("getMethods获取的方法：");
+        for(Method m:methods)
+            System.out.println(m);
+        //getDeclaredMethods()方法获取的所有方法
+        System.out.println("getDeclaredMethods获取的方法：");
+        for(Method m:declaredMethods)
+            System.out.println(m);
+    }
+    public static void test2() throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        Class<?> klass = methodClass.class;
+        //创建methodClass的实例
+        Object obj = klass.newInstance();
+        //获取methodClass类的add方法
+        Method method = klass.getMethod("add",int.class,int.class);
+        //调用method对应的方法 => add(1,4)
+        Object result = method.invoke(obj,1,4);
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        reflectDemo rf = new reflectDemo();
+        try {
+            rf.test();
+            rf.test2();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
+class methodClass {
+    public final int fuck = 3;
+    public int add(int a,int b) {
+        return a+b;
+    }
+    public int sub(int a,int b) {
+        return a+b;
+    }
+}
