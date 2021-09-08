4e50a0cb95e75204f961c9a6aa706057f8257e15
panj <pj_uestc@163.com>
2021-09-08 16:38:04 +0800

    add lc算法

--- structure&algorithm/src/squaresArray.java
+++ structure&algorithm/src/squaresArray.java
@@ -10,12 +10,22 @@ public class squaresArray {
         return res;
     }
     public int[] sortedSquares2(int[] nums) {
-        int n = nums.length;
-        int ans = new int[n];
-        for (int i = 0; i<n;i++){
-            if (nums[i]<0){
-
+        int left = 0;
+        int right = nums.length-1;
+        int [] res = new int[nums.length];
+        int write = nums.length-1;
+        while (left<=right){
+            if (nums[left]*nums[left]>nums[right]*nums[right]){
+                res[write] = nums[left]*nums[left];
+                left++;
+                write--;
+            }
+            else{
+                res[write] = nums[right]*nums[right];
+                right--;
+                write--;
             }
         }
+        return res;
     }
 }
