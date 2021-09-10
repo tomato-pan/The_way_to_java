c0c946302ffe93c740fed5fe386e1a87541ffa15
panj <pj_uestc@163.com>
2021-09-10 09:19:05 +0800

    add lc算法

--- structure&algorithm/src/moveZeroes.java
+++ structure&algorithm/src/moveZeroes.java
@@ -15,4 +15,35 @@ public class moveZeroes {
         nums[left] = nums[right];
         nums[right] = temp;
     }
+
+    public int[] twoSum(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; ++i) {
+            int low = i + 1, high = numbers.length - 1;
+            while (low <= high) {
+                int mid = (high - low) / 2 + low;
+                if (numbers[mid] == target - numbers[i]) {
+                    return new int[]{i + 1, mid + 1};
+                } else if (numbers[mid] > target - numbers[i]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+    public int[] twoSum1(int[] numbers, int target) {
+        int low = 0, high = numbers.length - 1;
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target) {
+                return new int[]{low + 1, high + 1};
+            } else if (sum < target) {
+                low++;
+            } else {
+                high--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
 }
