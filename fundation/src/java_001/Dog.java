4a7606947f879ea70f3c739441233b7c78f5824f
panj <260313538@qq.com>
2021-12-06 14:42:46 +0800

    add function

--- fundation/src/java_001/Dog.java
+++ fundation/src/java_001/Dog.java
@@ -1,13 +1,54 @@
 package java_001;
 
 public class Dog {
-    String breed;
-    int size;
-    String color;
-    int age;
+    private String name;
+    private int size;
+    private String color;
+    private int age;
 
-    void eat(){}
-    void run(){}
-    void sleep(){}
-    void name(){}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    void eat(){
+        System.out.println("eating!");
+    }
+    void run(){
+        System.out.println("running");
+    }
+    void sleep(){
+        System.out.println("sleeping");
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
