dbcb207300c52db795f3c596c87e80940b36220a
panj <260313538@qq.com>
2021-12-10 14:35:44 +0800

    add function

--- fundation/src/java_003/LC2.java
+++ fundation/src/java_003/LC2.java
@@ -16,31 +16,30 @@ public class ListNode {
         this.next = next;
     }
 
-    public class LC2 {
-        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-            ListNode ans = new ListNode();
-            while (!l1.next || !l2.next) {
-                System.out.println(l1.val + l2.val);
-                System.out.println(l1.val);
-                System.out.println(l2.val);
-            }
-            return ans;
+}
+public class LC2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ans = new ListNode();
+        while (l1.next !=null) {
+            System.out.println(l1.val + l2.val);
+            System.out.println(l1.val);
+            System.out.println(l2.val);
         }
-
-        public static void main(String[] args) {
-            LC2 lc2 = new LC2();
-            ListNode l1 = new ListNode(9);
-            ListNode l2= new ListNode(9);
-            l1.next=ListNode(9);
-            l1.next=ListNode(9);
-            l1.next=ListNode(9);
-            l1.next=ListNode(9);
-            l2.next=ListNode(9);
-            l2.next=ListNode(9);
-            l2.next=ListNode(9);
-            l2.next=ListNode(9);
-            lc2.addTwoNumbers(l1,l2);
-        }
+        return ans;
     }
 
+    public static void main(String[] args) {
+        LC2 lc2 = new LC2();
+        ListNode l1 = new ListNode(9);
+        ListNode l2= new ListNode(9);
+        l1.next= new ListNode(9);
+        l1.next.next=new ListNode(9);
+        l1.next.next.next=new ListNode(9);
+        l1.next.next.next.next=new ListNode(9);
+        l2.next=new ListNode(9);
+        l2.next.next=new ListNode(9);
+        l2.next.next.next=new ListNode(9);
+        l2.next.next.next.next=new ListNode(9);
+        lc2.addTwoNumbers(l1,l2);
+    }
 }
