9814855e983716ee9911e50cca13fd73e28ad0ac
panj <260313538@qq.com>
2021-12-15 14:23:58 +0800

    add arraylist+linkedlist

--- fundation/src/java_005/CollectDemo.java
+++ fundation/src/java_005/CollectDemo.java
@@ -1,25 +1,40 @@
 package java_005;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class CollectDemo {
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
         list.add("apple");
         list.add(null);
         list.add("banana");
         String s1 = list.get(1);
         System.out.println(s1);
+        list.set(2,"kiwifruit");
+        list.remove(1);
         System.out.println(list);
+        Collections.sort(list);// 排序
+        for (int i = 0; i < list.size() ; i++) {
+            System.out.println(list.get(i));
+        }
+        for (String ll:list) {
+            System.out.println(ll);
+        }
 
-        List<String> linkedlist = new LinkedList<>();
+        LinkedList<String> linkedlist = new LinkedList<>();
         linkedlist.add("google");
         linkedlist.add("baidu");
-        linkedlist.add("taobao");
-        linkedlist.add("weixin");
+        linkedlist.addFirst("wiki"); // 添加头部
+        linkedlist.addLast("wiki"); // 添加尾部
+        linkedlist.removeFirst(); // 移除头部
+        linkedlist.removeLast(); // 移除尾部
         System.out.println(linkedlist);
+        for (int i = 0; i < linkedlist.size() ; i++) {
+            System.out.println(linkedlist.get(i));
+        }
+        for (String ll:linkedlist) {
+            System.out.println(ll);
+        }
     }
 }
