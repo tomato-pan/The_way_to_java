b650380f2eaf5490539f4dd9e38253f6e203923d
panj <260313538@qq.com>
2021-12-27 16:13:40 +0800

    add LC

---
+++ structure&algorithm/src/tree/LC617.java
@@ -0,0 +1,36 @@
+package tree;
+
+public class LC617 {
+    int max = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return 0;
+        }
+        int leftSize = root.left == null ? 0 : dfs(root.left) + 1;
+        int rightSize = root.right == null ? 0 : dfs(root.right) + 1;
+        max = Math.max(max, leftSize + rightSize);
+        return Math.max(leftSize, rightSize);
+    }
+
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        TreeNode merged = new TreeNode(root1.val + root2.val);
+        merged.left = mergeTrees(root1.left, root2.left);
+        merged.right = mergeTrees(root1.right, root2.right);
+        return merged;
+    }
+}
