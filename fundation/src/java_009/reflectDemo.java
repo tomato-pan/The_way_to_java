16e5459131ca02295e8d852bb656d549fea8e92a
panj <260313538@qq.com>
2021-12-27 16:21:27 +0800

    add rf

--- fundation/src/java_009/reflectDemo.java
+++ fundation/src/java_009/reflectDemo.java
@@ -1,24 +1,47 @@
 package java_009;
 
-import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class reflectDemo implements InvocationHandler {
+public class reflectDemo {
     /**
      * 代理类中的真实对象
      */
-    private final Object target;
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchFieldException {
+        /**
+         * 获取TargetObject类的Class对象并且创建TargetObject类实例
+         */
+        Class<?> tagetClass = Class.forName("cn.javaguide.TargetObject");
+        TargetObject targetObject = (TargetObject) tagetClass.newInstance();
+        /**
+         * 获取所有类中所有定义的方法
+         */
+        Method[] methods = tagetClass.getDeclaredMethods();
+        for (Method method : methods) {
+            System.out.println(method.getName());
+        }
+        /**
+         * 获取指定方法并调用
+         */
+        Method publicMethod = tagetClass.getDeclaredMethod("publicMethod",
+                String.class);
 
-    public reflectDemo(Object target) {
-        this.target = target;
+        publicMethod.invoke(targetObject, "JavaGuide");
+        /**
+         * 获取指定参数并对参数进行修改
+         */
+        Field field = tagetClass.getDeclaredField("value");
+        //为了对类中的参数进行修改我们取消安全检查
+        field.setAccessible(true);
+        field.set(targetObject, "JavaGuide");
+        /**
+         * 调用 private 方法
+         */
+        Method privateMethod = tagetClass.getDeclaredMethod("privateMethod");
+        //为了调用private方法我们取消安全检查
+        privateMethod.setAccessible(true);
+        privateMethod.invoke(targetObject);
     }
 
-
-    public Object invoke(Object proxy, Method method, Object[] args) throws InvocationTargetException, IllegalAccessException {
-        System.out.println("before method " + method.getName());
-        Object result = method.invoke(target, args);
-        System.out.println("after method " + method.getName());
-        return result;
-    }
 }
