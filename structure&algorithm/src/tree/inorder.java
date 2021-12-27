256a3c4345ca6bfce3e192fdf8a67c902380c04a
panj <260313538@qq.com>
2021-12-27 14:11:33 +0800

    add LC

--- structure&algorithm/src/tree/inorder.java
+++ structure&algorithm/src/tree/inorder.java
@@ -5,12 +5,47 @@ import java.util.List;
 
 public class inorder {
     ArrayList<Integer> list = new ArrayList<Integer>();
+
     public List<Integer> inorderTraversal(TreeNode root) {
-        if(root == null) {return new ArrayList<Integer>();}
+        if (root == null) {
+            return new ArrayList<Integer>();
+        }
         inorderTraversal(root.left);
         list.add(root.val);
         inorderTraversal(root.right);
         return list;
     }
 
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+
+    public boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t4 = null;
+        TreeNode t2 = new TreeNode(3);
+        TreeNode t3 = new TreeNode(4);
+        t1.left = t2;
+        t1.right = t3;
+        inorder abc = new inorder();
+        System.out.println(abc.maxDepth(t1));
+        System.out.println(abc.maxDepth(t4));
+    }
 }
