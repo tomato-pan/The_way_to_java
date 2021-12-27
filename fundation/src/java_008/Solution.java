7c070eafadb665ed98367da393f9d70af0a431bb
panj <260313538@qq.com>
2021-12-27 09:45:41 +0800

    add LC

--- fundation/src/java_008/Solution.java
+++ fundation/src/java_008/Solution.java
@@ -1,21 +1,21 @@
 package java_008;
+
 // LC686
 public class Solution {
     public int repeatedStringMatch(String a, String b) {
-        int l = (b.length() + a.length() - 1)/a.length();
-        int count=0;
+        int l = (b.length() + a.length() - 1) / a.length();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < l; i++) {
             sb.append(a);
         }
-        for(int i=0;i<=sb.length()-b.length();i++){
-            if(sb.substring(i, i + b.length()).equals(b))
+        for (int i = 0; i <= sb.length() - b.length(); i++) {
+            if (sb.substring(i, i + b.length()).equals(b))
                 return l;
         }
         sb.append(a);
-        for(int i=a.length()*l-b.length()+1;i<=sb.length()-b.length();i++)
-            if(sb.substring(i, i + b.length()).equals(b))
-                return l+1;
+        for (int i = a.length() * l - b.length() + 1; i <= sb.length() - b.length(); i++)
+            if (sb.substring(i, i + b.length()).equals(b))
+                return l + 1;
         return -1;
     }
 }
