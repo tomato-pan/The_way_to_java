c5b798ec6a85618c7fdbce51feed259e78b5e1a6
panj <260313538@qq.com>
2022-02-08 10:15:51 +0800

    add interface

---
+++ fundation/src/javaDesignPattern/FactoryDemo.java
@@ -0,0 +1,44 @@
+package javaDesignPattern;
+
+public class FactoryDemo {
+    public static void main(String[] args) {
+        SimpleFactory.makeProduct(1).show();
+        SimpleFactory.makeProduct(0).show();
+    }
+
+    public interface Product {
+        void show();
+    }
+
+    static class ConcreteProduct1 implements Product {
+        @Override
+        public void show() {
+            System.out.println("具体产品1！");
+        }
+    }
+
+    static class ConcreteProduct2 implements Product {
+        @Override
+        public void show() {
+            System.out.println("具体产品2！");
+        }
+    }
+
+    final class Const {
+        static final int PRODUCT_A = 0;
+        static final int PRODUCT_B = 1;
+        static final int PRODUCT_C = 2;
+    }
+
+    static class SimpleFactory {
+        public static Product makeProduct(int kind) {
+            switch (kind) {
+                case Const.PRODUCT_A:
+                    return new ConcreteProduct1();
+                case Const.PRODUCT_B:
+                    return new ConcreteProduct2();
+            }
+            return null;
+        }
+    }
+}
