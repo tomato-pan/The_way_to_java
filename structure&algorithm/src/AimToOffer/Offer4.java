a11280c1b44e6c55bf5c583398c16518783fe32d
panj <pj_uestc@163.com>
2022-06-20 21:59:13 +0800

    AimToOffer

--- structure&algorithm/src/AimToOffer/Offer4.java
+++ structure&algorithm/src/AimToOffer/Offer4.java
@@ -3,10 +3,11 @@ package AimToOffer;
 import leetcode.editor.cn.Solution929;
 
 public class Offer4 {
+    //暴力
     public static boolean findNumberIn2DArray(int[][] matrix, int target) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] == target) {
+        for (int[] ints : matrix) {
+            for (int j = 0; j < ints.length; j++) {
+                if (ints[j] == target) {
                     return true;
                 }
             }
@@ -15,7 +16,13 @@ public class Offer4 {
     }
 
     public static boolean findNumberIn2DArray1(int[][] matrix, int target) {
-
+        int i = matrix.length - 1, j = 0;
+        while(i >= 0 && j < matrix[0].length)
+        {
+            if(matrix[i][j] > target) i--;
+            else if(matrix[i][j] < target) j++;
+            else return true;
+        }
         return false;
     }
 
