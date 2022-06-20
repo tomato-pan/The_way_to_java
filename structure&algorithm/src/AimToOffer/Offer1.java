a418fc71cd8290004fe174be36484d3041d23b69
panj <pj_uestc@163.com>
2022-06-20 22:09:27 +0800

    AimToOffer

--- structure&algorithm/src/AimToOffer/Offer1.java
+++ structure&algorithm/src/AimToOffer/Offer1.java
@@ -1,4 +1,16 @@
 package AimToOffer;
 
+import java.util.HashMap;
+
 public class Offer1 {
+    public int findRepeatNumber(int[] nums) {
+        HashMap hasKey = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            if (hasKey.get(nums[i]) != null) {
+                return nums[i];
+            }
+            hasKey.put(nums[i], i);
+        }
+        return -1;
+    }
 }
